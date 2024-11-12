c7fe7afe5c912bc59d4f8b19916f64cf92ac4af7
BalderHolst <balderwh@gmail.com>
2024-11-12 07:26:01 +0100

    demo: Controls

--- demo/index.js
+++ demo/index.js
@@ -1,33 +1,64 @@
 import init, { render, get_buffer_ptr, get_buffer_size, canvas_width, canvas_height } from "./pkg/ssl_demo.js";
 
+let render_to_canvas = undefined;
 
 init().then((wasm) => {
-    render("(0.5 - (x*x+y*y)*(x*x+y*y)*5) + {0.5, x-0.5, y+0.5}");
 
-    const ptr = get_buffer_ptr();
-    const size = get_buffer_size();
-    const memory = new Uint8Array(wasm.memory.buffer);
+    render_to_canvas = (code) => {
+        console.log("Running code: ", code);
 
-    const canvasElement = document.querySelector("canvas");
-    const canvasContext = canvasElement.getContext("2d");
+        render(code);
 
-    const width = canvas_width();
-    const height = canvas_height();
+        const ptr = get_buffer_ptr();
+        const size = get_buffer_size();
+        const memory = new Uint8Array(wasm.memory.buffer);
 
+        const canvasElement = document.querySelector("canvas");
+        const canvasContext = canvasElement.getContext("2d");
 
-    const imageData = canvasContext.createImageData(
-        width, height
-    );
+        const width = canvas_width();
+        const height = canvas_height();
 
-    canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+        const imageData = canvasContext.createImageData(
+            width, height
+        );
 
-    const imageDataArray = memory.slice(
-        ptr,
-        ptr + size
-    );
+        canvasContext.canvas.width = width;
+        canvasContext.canvas.height = height;
+        canvasContext.clearRect(0, 0, width, height);
 
-    imageData.data.set(imageDataArray);
-    canvasContext.putImageData(imageData, 0, 0);
+        const imageDataArray = memory.slice(
+            ptr,
+            ptr + size
+        );
 
+        imageData.data.set(imageDataArray);
+        canvasContext.putImageData(imageData, 0, 0);
+    };
+    render_to_canvas("Stupid Shader Language");
+});
 
+// Run button
+document.getElementById("run").addEventListener("click", () => {
+    const code = document.getElementById("code").value;
+    if (render_to_canvas) render_to_canvas(code);
+});
+
+// Download button
+document.getElementById("download").addEventListener("click", () => {
+    let uri = document.querySelector("canvas").toDataURL();
+    let link = document.createElement("a");
+    link.download = "ssl-output.png";
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+// Run code on ctrl-enter
+document.getElementById("code").addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.key === "Enter") {
+        const code = document.getElementById("code").value;
+        if (render_to_canvas) render_to_canvas(code);
+    }
 });
